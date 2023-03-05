6fca81bed7f6fc66aa25e183b6ac96efaf1d93d8
GenryEden <a926788@gmail.com>
2023-03-05 17:51:44 +0300

    documented Multiplexor

--- RossEventMultiplexor.py
+++ RossEventMultiplexor.py
@@ -1,33 +1,46 @@
 import threading
-from typing import List
+from typing import List, Set
 
 from MessagersInterfaces import Listener, T, Notifier
 from RossServer import RossEvent
 
 
 class Multiplexor(Listener[RossEvent], Notifier[RossEvent]):
+    """
+        class that listens to one Listener[RossEvent] and notifies multiple Listeners[RossEvent]
+    """
     def __init__(self):
-        self._listenerList: List[Listener[RossEvent]] = []
+        self._listenerSet: Set[Listener[RossEvent]] = set()
         self._listMutex = threading.Lock()
 
     def add_listener(self, listener: Listener[RossEvent]):
+        """
+        add a listener that will be notified by the multiplexor
+
+        :param listener: listener to add
+        :type listener: Listener[RossEvent]
+        """
         with self._listMutex:
-            self._listenerList.append(listener)
-            return len(self._listenerList) - 1
+            self._listenerSet.add(listener)
 
     def on_message(self, message: RossEvent, notifier: Notifier[RossEvent]):
         with self._listMutex:
-            for listener in self._listenerList:
+            for listener in self._listenerSet:
                 listener(message, self)
 
-    def get_listeners(self):
+    def get_listeners(self) -> Set[Listener[RossEvent]]:
+        """
+        :return: set of listener that multiplexor notifies
+        """
         with self._listMutex:
-            return self._listenerList.copy()
-
-    def delete_listener_by_id(self, index: int) -> Listener[RossEvent]:
-        with self._listMutex:
-            return self._listenerList.pop(index)
+            return self._listenerSet.copy()
 
     def delete_listener(self, listener: Listener[RossEvent]):
+        """
+        removes listener from set of listeners to be notified
+
+        :param listener: listener to not notify anymore
+        :return:
+        """
         with self._listMutex:
-            self._listenerList.remove(listener)
+            self._listenerSet.remove(listener)
