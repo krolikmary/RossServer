ae1e08c4ab889eab010e116fb4a1b751326e141f
GrishaRubitel <post.germa@gmail.com>
2023-03-19 19:47:05 +0300

    Added "JSONEncoder.py" and example file "UMD2LogJSON.py"

--- examples/UMD2LogJSON.py
+++ examples/UMD2LogJSON.py
@@ -6,18 +6,19 @@ Scheme:
 socket - - - > UDPServer -------> UMDDecoder ----------> ListenerLogger - - - > loguru
 """
 
-
 if __name__ == "__main__":
     import sys
 
     sys.path.append('../')
     from UDPServer import UDPServer
+    from JSONEncoder import RossEventToJson
     from RossServer import RossDecoder
     from UMDDecoder import UMDDecoder
     from UtilityDecoders import ListenerLogger
 
     listenerLogger = ListenerLogger()
-    rossDecoder = RossDecoder(listenerLogger)
+    jsonEncoder = RossEventToJson(listenerLogger)
+    rossDecoder = RossDecoder(jsonEncoder)
     umdDecoder = UMDDecoder(rossDecoder)
     udpServer = UDPServer(umdDecoder, host="127.0.0.1", port=1337)
     udpServer.run()
