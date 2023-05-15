3ed6b60ef388f9ae5e8b1e5626f5d0c350aa1ad8
frzai <mashzuk@gmail.com>
2023-05-15 20:22:37 +0300

    привет

--- UI/main.py
+++ UI/main.py
@@ -1,18 +1,20 @@
-
-import Model
-from collections import namedtuple
-
+import sys
 from loguru import logger as lg
 from flask import Flask, render_template, url_for, redirect, request
+from collections import namedtuple
 
+sys.path.append("../")
+
+import Model
+from ServerDescriptor import OutputProto
 
 app = Flask(__name__)
 
 Server = namedtuple('ID', 'ID port protocol')
 servers = Model.ServersModel("127.0.0.1")
 servers.run()
-servers.add_tslumd(7355)
-key = 0
+servers.add_sound("../sounds/")
+servers.add_filtered_eztslumd(1234, {1, 2})
 
 
 @app.route('/', methods=['GET'])
@@ -22,21 +24,35 @@ def main():
 
 @app.route('/add_message', methods=['POST'])
 def add_message():
-
-    port = request.form['port']
+    filtered_cameras = (request.form['filtered_cameras'])
+    filtered_cameras_set = set(map(int, filtered_cameras.split()))
+    for i in filtered_cameras_set:
+        if i < 0 or i > 128:
+            return redirect(url_for('main'))
+    port = int(request.form['port'])
     protocol = request.form['protocol']
     lg.debug(f"Got protocol {protocol}")
-    if port < "0" or port > "65536":
+    if port < 0 or port > 65536:
         return redirect(url_for('main'))
-    else:
-        if protocol == "sound":
-            servers.add_sound("../sounds/")
-        if protocol == "TSL UMD v3.1":
-            servers.add_tslumd(port)
-        if protocol == "JSON":
-            servers.add_json(port)
-
-        return redirect(url_for('main'))
+    for descriptor in servers.get_descriptors().values():
+        if descriptor.port == port:
+            return redirect(url_for('main'))
+    if protocol == "sound":
+        for descriptor in servers.get_descriptors().values():
+            if descriptor.protocol == OutputProto.SOUND:
+                return redirect(url_for('main'))
+        servers.add_sound("../sounds/")
+    if protocol == "TSL UMD v3.1":
+        servers.add_tslumd(port)
+    if protocol == "JSON":
+        servers.add_json(port)
+    if protocol == "ezTSL UMD":
+        servers.add_eztslumd(port)
+    if protocol == "filtered ezTSL UMD":
+        servers.add_filtered_eztslumd(port, filtered_cameras_set)
+    if protocol == "filtered TSL UMD":
+        servers.add_filtered_tslumd(port, filtered_cameras_set)
+    return redirect(url_for('main'))
 
 
 @app.route('/delete', methods=['POST'])
